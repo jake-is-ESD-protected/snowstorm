f693ced454e3626d9adea7ed7b49324ce63edcbb
jake-is-ESD-protected <jts4681@students.fhv.at>
2022-12-16 18:18:55 +0100

    added updates OTA

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #include <Arduino.h>
 #include "canvas.h"
 #include "esp_timer.h"
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <AsyncElegantOTA.h>
+#include "passwords.h"
+
+AsyncWebServer server(80);
 
 c_canvas canvas(OLED_WIDTH, OLED_HEIGHT);
 c_snowstorm storm;
@@ -8,6 +15,21 @@ c_snowstorm storm;
 void setup()
 {
   Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(SSID, PSSWD);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    log_e(".");
+  }
+  Serial.println(WiFi.localIP());
+
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+            { request->send(200, "text/plain", "Snowstorm access point. See './update' for deployable updates."); });
+
+  AsyncElegantOTA.begin(&server);
+  server.begin();
+
   canvas.init();
   canvas.clear();
   canvas.show();
